67db807373657eae865b3a781ba769b8f57e4770
Bart Claessens <bart.cla@gmail.com>
2015-04-03 23:13:54 +0200

    Included annyang for speech recognition

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -19,7 +19,45 @@ Application.prototype = {
 
     // add resize listener
     $(window).resize( function() {
-      this.scene.resize();
+      //this.scene.resize();
     }.bind( this ) );
+
+
+    // listen to what the user has to say
+    if (annyang) {
+      var commands = {
+        'drop tree': function() {
+          this.scene.dropTrees(1);
+        }.bind( this ),
+
+        /*
+        'drop :amount trees': function( amount ) {
+          this.scene.dropTrees(amount);
+        }.bind( this ),
+        */
+
+        'drop some trees': function() {
+          this.scene.dropTrees(_.random(3, 10));
+        }.bind( this ),
+
+        'drop forest': function() {
+          this.scene.dropTrees(30);
+        }.bind( this ),
+
+        'remove some trees': function() {
+          this.scene.eraseTrees(_.random(3, 10));
+        }.bind( this ),
+
+        'suck a cock': function() {
+          window.location = 'https://www.youtube.com/watch?v=tiuO8m0_aeQ';
+        }
+      };
+
+      // add our commands to annyang
+      annyang.addCommands(commands);
+
+      // start listening
+      annyang.start();
+    }
   },
 };
