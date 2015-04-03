1d66d21fdf331ff616e8b67ef481b8ba85af7cde
Bart Claessens <bart.cla@gmail.com>
2015-04-03 23:38:44 +0200

    Some logging for speach recog

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -27,24 +27,27 @@ Application.prototype = {
     if (annyang) {
       var commands = {
         'drop tree': function() {
+          console.log('drop tree');
           this.scene.dropTrees(1);
         }.bind( this ),
 
-        /*
         'drop :amount trees': function( amount ) {
+          console.log('drop ' + amount + ' trees');
           this.scene.dropTrees(amount);
         }.bind( this ),
-        */
 
         'drop some trees': function() {
+          console.log('drop some trees');
           this.scene.dropTrees(_.random(3, 10));
         }.bind( this ),
 
         'drop forest': function() {
+          console.log('drop forest');
           this.scene.dropTrees(30);
         }.bind( this ),
 
         'remove some trees': function() {
+          console.log('remove some trees');
           this.scene.eraseTrees(_.random(3, 10));
         }.bind( this ),
 
@@ -56,8 +59,18 @@ Application.prototype = {
       // add our commands to annyang
       annyang.addCommands(commands);
 
+      annyang.addCallback('start', function() {  console.log('start'); });
+      annyang.addCallback('error', function() {  console.log('error'); });
+      annyang.addCallback('end', function() {  console.log('end'); });
+      annyang.addCallback('result', function() {  console.log('result'); });
+      annyang.addCallback('resultMatch', function() {  console.log('resultMatch'); });
+      annyang.addCallback('resultNoMatch', function() {  console.log('resultNoMatch'); });
+      annyang.addCallback('errorNetwork', function() {  console.log('errorNetwork'); });
+      annyang.addCallback('errorPermissionBlocked', function() {  console.log('errorPermissionBlocked'); });
+      annyang.addCallback('errorPermissionDenied', function() {  console.log('errorPermissionDenied'); });
+
       // start listening
-      annyang.start();
+      annyang.start({continuous:true});
     }
   },
 };
